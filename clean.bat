f80d4862dafaca31e20a0ce76b7ebcbc6c94aaad
Cathal Noonan <cathalnoonan@live.com>
2021-04-11 17:41:56 +0100

    Fixed clean script

--- clean.bat
+++ clean.bat
@@ -3,7 +3,7 @@
 CALL npx rimraf ./control/node_modules
 CALL npx rimraf ./control/obj
 CALL npx rimraf ./control/out
-CALL npx rimraf ./control/ProcessSessions/out
+CALL npx rimraf ./control/CharactersRemaining/out
 
 CALL npx rimraf ./solution/bin
 CALL npx rimraf ./solution/obj
