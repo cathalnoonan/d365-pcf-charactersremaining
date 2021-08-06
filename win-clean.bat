07268158ca2f1e0f9f9643119bbf1d52b8589a19
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2021-08-06 21:49:06 +0100

    Updated build scripts (windows/mac)

---
+++ win-clean.bat
@@ -0,0 +1,10 @@
+@echo off
+
+call npx rimraf ./control/node_modules
+call npx rimraf ./control/obj
+call npx rimraf ./control/out
+call npx rimraf ./control/CharactersRemaining/out
+
+call npx rimraf ./solution/bin
+call npx rimraf ./solution/obj
+call npx rimraf ./solution/dist
