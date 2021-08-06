c52c1e1a3caf95b3cb1316edb6c0ef3698dcf362
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2021-08-07 00:43:46 +0100

    Handle control being disabled

--- control/CharactersRemaining/components/charactersremaining.tsx
+++ control/CharactersRemaining/components/charactersremaining.tsx
@@ -5,109 +5,65 @@ export interface CharactersRemainingComponentProps {
     numberOfLines: number
     allowedNumberOfCharacters: number
     disabled: boolean
-    notifyOutputChanged: () => void
+    notifyOutputChanged: (newValue: string) => void
     formatNumber: (n: number) => string
 }
 
-interface CharactersRemainingComponentState {
-    active: boolean
-    cssActive: boolean
-    value: string | null
-}
+export const CharactersRemainingComponent = (props: CharactersRemainingComponentProps) => {
 
-export class CharactersRemainingComponent extends React.Component<CharactersRemainingComponentProps, CharactersRemainingComponentState> {
+    const [value, setValue] = React.useState(props.value)
+    const [active, setActive] = React.useState(false)
 
-    private textarea: React.RefObject<HTMLTextAreaElement>
-    
-    constructor(props: CharactersRemainingComponentProps) {
-        super(props)
-        this.textarea = React.createRef<HTMLTextAreaElement>()
-        this.state = {
-            active: false,
-            cssActive: false,
-            value: props.value,
+    const error = value.length > props.allowedNumberOfCharacters
+    const formattedCharactersRemaining = props.formatNumber(props.allowedNumberOfCharacters - value.length)
+
+    function onChange(element: React.ChangeEvent<HTMLTextAreaElement>): void {
+        const newValue = element.currentTarget.value
+        setValue(newValue)
+        props.notifyOutputChanged(newValue)
+    }
+
+    function onKeyPress(ev: React.KeyboardEvent) {
+        if (props.numberOfLines === 1 && ev.key === 'Enter') {
+            ev.preventDefault()
         }
     }
 
-    render() {
-        return (
-            <div
-                className={'characters-remaining ' + this.getActiveClass() + this.getErrorClass()}
-                onClick={this.onClick}
-            >
-                <div className='wrapper'>
-                    <textarea
-                        value={this.state.value ?? ''}
-                        placeholder={this.state.active ? '' : '---'}
-                        onChange={this.onChange}
-                        onFocus={this.onFocus}
-                        onBlur={this.onBlur}
-                        onKeyPress={this.onKeyPress}
-                        className={this.getControlTypeClass() + this.getActiveClass() + this.getErrorClass()}
-                        rows={this.props.numberOfLines}
-                        autoComplete='off'
-                        autoCorrect='off'
-                        autoCapitalize='false'
-                        spellCheck={this.state.active}
-                        ref={this.textarea}
-                        disabled={this.props.disabled}
-                    />
-                    <div className={'label-container' + this.getHiddenClass()}>
-                        <hr />
-                        <label className={this.getHiddenClass() + this.getErrorClass()}>
-                            {this.getFormattedCharactersRemaining()}
-                        </label>
-                    </div>
+    if (props.disabled) return (
+        <div className='wrapper disabled'>
+            <div className='characters-remaining'>
+                <textarea value={value}
+                    placeholder={'---'}
+                    disabled={props.disabled}
+                    rows={props.numberOfLines} />
+            </div>
+        </div>
+    )
+
+    return (
+        <div className='wrapper'>
+            <div className={`characters-remaining ${active ? 'active' : ''} ${error ? 'error' : ''}`.trim()}
+                onClick={() => setActive(true)}>
+                <textarea value={value}
+                    placeholder={active ? '' : '---'}
+                    onChange={onChange}
+                    onKeyPress={onKeyPress}
+                    onFocus={() => setActive(true)}
+                    onBlur={() => setActive(false)}
+                    className={`${props.numberOfLines === 1 ? 'input' : 'textarea'} ${active ? 'active' : ''} ${error ? 'error' : ''}`}
+                    rows={props.numberOfLines}
+                    autoComplete='off'
+                    autoCorrect='off'
+                    autoCapitalize='false'
+                    spellCheck={active}
+                />
+                <div className={`label-container ${active ? '' : 'hidden'}`.trim()}>
+                    <hr />
+                    <label className={`${active ? '' : 'hidden'} ${error ? 'error' : ''}`.trim()}>
+                        {formattedCharactersRemaining}
+                    </label>
                 </div>
             </div>
-        )
-    }
-
-    // State
-    getValue = (): string | null => this.state.value
-    setValue = (value: string | null) => {
-        // Mediate whether the value should be updated in state.
-        // Without this check, the value could be overwritten by an old value received by the PCF framework.
-        if (this.state.active) return
-        this.setValueInternal(value)
-    }
-    private setValueInternal = (value: string | null): void => {
-        this.setState({ value }, this.props.notifyOutputChanged)
-    }
-    private getRemainingNumberOfCharacters = (): number => {
-        return this.props.allowedNumberOfCharacters - (this.getValue()?.length || 0)
-    }
-
-    // CSS Class Names
-    private getErrorClass = (): string => this.getRemainingNumberOfCharacters() < 0 ? ' error' : ''
-    private getActiveClass = (): string => this.state.cssActive ? ' active' : ''
-    private getControlTypeClass = (): string => this.props.numberOfLines === 1 ? 'input' : 'textarea'
-    private getHiddenClass = (): string => this.state.active ? '' : ' hidden'
-
-    // Event handlers
-    private onChange = (): void => this.setValueInternal(this.textarea.current?.value ?? null)
-    private onClick = (): void => {
-        if (this.props.disabled) return
-        
-        this.textarea.current?.focus()
-    }
-    private onFocus = (): void => {
-        if (this.props.disabled) return
-
-        // HACK: React setState is not synchronous.
-        //
-        // Make the CSS class display as active, but don't make the control consider itself as active
-        // until a delay has happened.
-        // This allows tabbing onto the control in a way that the new value will still be received.
-        this.setState({ cssActive: true })
-        setTimeout(() => this.setState({ active: true }), 50)
-    }
-    private onBlur = (): void => this.setState({ active: false, cssActive: false })
-    private onKeyPress = (ev: React.KeyboardEvent): void => {
-        // Single line of text: Prevent new lines
-        if (this.props.numberOfLines === 1 && ev.charCode === 13) ev.preventDefault()   // TODO: charCode Deprecated
-    }
-
-    // Utility
-    private getFormattedCharactersRemaining = (): string => this.props.formatNumber(this.getRemainingNumberOfCharacters())
+        </div>
+    )
 }
--- control/CharactersRemaining/index.ts
+++ control/CharactersRemaining/index.ts
@@ -7,41 +7,38 @@ import { IInputs, IOutputs } from './generated/ManifestTypes'
 export class CharactersRemaining implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
     private container: HTMLDivElement
-    private charactersRemaining: CharactersRemainingComponent
+    private notifyOutputChanged: () => void
+    private value: string
 
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
+    public init(context: ComponentFramework.Context<IInputs>, 
+                notifyOutputChanged: () => void, 
+                state: ComponentFramework.Dictionary, 
+                container: HTMLDivElement) {
         
-        const value: string = context.parameters.value.raw || ''
-        const numberOfLines: number = context.parameters.numberOfLines.raw || 1
-        const allowedNumberOfCharacters: number = context.parameters.value.attributes?.MaxLength || 100
-        const disabled = context.mode.isControlDisabled
-        const props: CharactersRemainingComponentProps = {
-            value,
-            numberOfLines,
-            allowedNumberOfCharacters,
-            disabled,
-            notifyOutputChanged,
-            formatNumber: (n: number): string => context.formatting.formatInteger(n),
-        }
         this.container = container
-
-        this.charactersRemaining = ReactDOM.render(
-            React.createElement(
-                CharactersRemainingComponent,
-                props
-            ),
-            this.container
-        )
+        this.notifyOutputChanged = notifyOutputChanged
     }
 
     public updateView(context: ComponentFramework.Context<IInputs>): void {
         // Add code to update control view
-        this.charactersRemaining.setValue(context.parameters.value.raw)
+        const props: CharactersRemainingComponentProps = {
+            value: context.parameters.value.raw || '',
+            numberOfLines: context.parameters.numberOfLines.raw || 1,
+            allowedNumberOfCharacters: context.parameters.value.attributes?.MaxLength || 100,
+            disabled: context.mode.isControlDisabled,
+            notifyOutputChanged: (newValue: string) => (this.value = newValue, this.notifyOutputChanged()),
+            formatNumber: (n: number): string => context.formatting.formatInteger(n),
+        }
+
+        ReactDOM.render(
+            React.createElement(CharactersRemainingComponent, props),
+            this.container
+        )
     }
 
     public getOutputs(): IOutputs {
         return {
-            value: this.charactersRemaining.getValue() ?? undefined,
+            value: this.value
         }
     }
 
