5457b9ab60e0ba7956eda42860b9c41288e7c9f1
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2022-04-27 22:24:14 +0100

    Build related updates
    - Update npm packages
    - Update build scripts
    - Update github action workflow

---
+++ control/webpack.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type { import('webpack').Configuration }
+ */
+ module.exports = {
+  // Generate a source map to make debugging easier.
+  devtool: 'source-map',
+
+  // Support older browsers.
+  target: ['web', 'es5'],
+}
